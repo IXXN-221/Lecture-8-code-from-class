5efaaa3180d89f1d6226e7661fe62ef9c3a15c06
EmilSchroder <eschro66@gmail.com>
2019-09-09 10:54:24 +1200

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let data = [
+        {
+            mountain: 'Everest',
+            hoursTaken: [90,93,88,94],
+            summary: 'Tough but fair'
+        },
+        {
+            film: 'K2',
+            hoursTaken: [65,73,67,66],
+            summary: 'Easy'
+        },
+        {
+            film: 'Aoraki',
+            hoursTaken: [50,62,74,80],
+            summary: 'A stroll'
+        }
+
+    ]
